85356305ff3591df5c050f4fcffad706b2a213e4
sharunvictor-goatrobotics <sharun@goat-robotics.com>
2025-04-24 15:27:22 +0530

    handle param change in hpp removed

--- graceful_controller_ros/include/graceful_controller_ros/graceful_controller_ros.hpp
+++ graceful_controller_ros/include/graceful_controller_ros/graceful_controller_ros.hpp
@@ -232,12 +232,6 @@
  
      rcl_interfaces::msg::SetParametersResult onParameterChange(
      const std::vector<rclcpp::Parameter>& parameters);
-     {
-         // Handle parameter change
-         rcl_interfaces::msg::SetParametersResult result;
-         result.successful = true;
-         return result;
-     }
    };
  
    /**
